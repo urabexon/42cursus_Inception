62c544abdd094ecf0b78ad993040a8194438e56c
urabexon <hiroki708.a48uk@gmail.com>
2025-04-15 14:56:20 +0900

    nginx init.sh設定 秘密鍵 公開鍵の自動設定

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -3,11 +3,29 @@
 #                                                         :::      ::::::::    #
 #    init.sh                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+         #
+#    By: urabex <urabex@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/04/12 18:56:26 by hurabe            #+#    #+#              #
-#    Updated: 2025/04/12 18:58:40 by hurabe           ###   ########.fr        #
+#    Updated: 2025/04/15 14:53:31 by urabex           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
+
+# エラー発生時に即終了させる
+set -e
+
+# SSL証明書の保存先移動
+cd /etc/nginx/ssl
+
+# 秘密鍵作成(NISTなどのセキュリティ機関が2048bit以上を推奨しており、パフォーマンスバランスがいいためこちらを指定)
+openssl genrsa -out inception.key 2048
+
+# CSR(証明書署名要求)作成
+openssl req -new -key inception.key -out inception.csr -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=hurabe.42.fr"
+
+# CSRから自己署名証明書を作成
+openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
+
+# Nginx実行
+exec nginx -g 'daemon off;'
